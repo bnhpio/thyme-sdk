ccc3864072909b3b64a7072defa3bf409847e67d
Jakhongir Tashpulatov <tashpulatov.jakhongir@gmail.com>
2025-11-06 02:13:05 +0100

    chore: update rslib

--- packages/sdk/rslib.config.ts
+++ packages/sdk/rslib.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
+  source: {
+    entry: {
+      index: './src/index.ts',
+      'task/archive': './src/task/archive/index.ts',
+      'task/transaction': './src/task/transaction/index.ts',
+      'task/schema': './src/task/schema/index.ts',
+    },
+  },
   lib: [
     {
       format: 'esm',
       syntax: ['node 18'],
       dts: true,
-    },
-    {
-      format: 'cjs',
-      syntax: ['node 18'],
     },
   ],
 });

de8e97482b35aee3e46400ad41e8c9f21adecb45
Jakhongir Tashpulatov <tashpulatov.jakhongir@gmail.com>
2025-11-17 13:51:48 +0100

    docs: add docs

---
+++ apps/docs/vocs.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vocs';
+
+export default defineConfig({
+  title: 'Thyme Docs',
+  sidebar: [
+    {
+      text: 'Thyme',
+      link: '/getting-started',
+    },
+    {
+      text: 'Example',
+      link: '/example',
+    },
+    {
+      text: 'Thyme CLI',
+      link: '/cli',
+      items: [
+        {
+          text: 'Quick Start',
+          link: '/cli/quick-start',
+        },
+        {
+          text: 'Commands',
+          link: '/cli/commands',
+          items: [
+            {
+              text: 'thyme create',
+              link: '/cli/commands/thyme-create',
+            },
+            {
+              text: 'thyme auth',
+              link: '/cli/commands/thyme-auth',
+            },
+            {
+              text: 'thyme simulate',
+              link: '/cli/commands/thyme-simulate',
+            },
+            {
+              text: 'thyme upload',
+              link: '/cli/commands/thyme-upload',
+            },
+          ],
+        },
+        {
+          text: 'Configuration',
+          link: '/cli/configuration',
+        },
+      ],
+    },
+    {
+      text: 'Thyme SDK',
+      link: '/thyme-sdk',
+    },
+    {
+      text: 'CLI + SDK',
+      link: '/cli-sdk',
+    },
+  ],
+});
